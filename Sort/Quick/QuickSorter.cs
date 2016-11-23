571c3f7b550859688055b2081e94e54b265bd0a1
Alexey <alexey@azelenin.work>
2016-11-22 22:16:44 -0500

    Implement the quick sort algorithm

--- Sort/Quick/QuickSorter.cs
+++ Sort/Quick/QuickSorter.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Algorithm.Sort.Common;
 
 namespace Algorithm.Sort.Quick {
@@ -12,7 +8,41 @@ namespace Algorithm.Sort.Quick {
         public QuickSorter(T[] initArray) : base(initArray) {}
 
         public override T[] GetSorted() {
-            throw new NotImplementedException();
+            var sorted = new T[Container.Length];
+            Container.CopyTo(sorted, 0);
+            QuickSort(sorted, 0, sorted.Length - 1);
+            return sorted;
+        }
+
+        private void QuickSort(T[] elements, int leftIndex, int rightIndex) {
+            int i = leftIndex, j = rightIndex;
+            var pivot = elements[(leftIndex + rightIndex)/2];
+
+            while (i <= j) {
+                while (elements[i].CompareTo(pivot) < 0) {
+                    i++;
+                }
+
+                while (elements[j].CompareTo(pivot) > 0) {
+                    j--;
+                }
+
+                if (i <= j) {
+                    Swap(elements, i, j);
+
+                    i++;
+                    j--;
+                }
+            }
+
+            // Recursive calls
+            if (leftIndex < j) {
+                QuickSort(elements, leftIndex, j);
+            }
+
+            if (i < rightIndex) {
+                QuickSort(elements, i, rightIndex);
+            }
         }
     }
 }
